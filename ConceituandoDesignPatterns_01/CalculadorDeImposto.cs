ba220fe7858f91d764c599c13a9120e192174475
Francisco Oliveira <fransoliveira@gmail.com>
2022-01-20 09:49:39 -0300

    Add project files.

---
+++ ConceituandoDesignPatterns_01/CalculadorDeImposto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceituandoDesignPatterns_01
+{
+    public class CalculadorDeImposto
+    {
+        public void RealizaCalculo(Orcamento orcamento, String imposto) 
+        {  
+            double icms = orcamento.Valor * 0.1;
+            Console.WriteLine(icms);
+
+            double iss = orcamento.Valor * 0.06;
+            Console.WriteLine(iss);
+        }
+    }
+}
---
+++ ConceituandoDesignPatterns_01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConceituandoDesignPatterns_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
